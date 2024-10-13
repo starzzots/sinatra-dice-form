0338b4e1e31dd2a412d025ecf0968a3968507d50
Kyle Verwey <108106818+starzzots@users.noreply.github.com>
2024-10-13 23:40:40 +0000

    and another one down :D

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem "puma", "~> 5.0"
 
 # use active record
 gem "sinatra-activerecord"
+gem "http"
 
 group :development do
   gem "better_errors"
--- app.rb
+++ app.rb
@@ -2,8 +2,15 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/process_roll") do
+  @dice = params.fetch("dice").to_i
+  @sides = params.fetch("sides").to_i
+  @rolls = []
+  @dice.times do 
+    @rolls << rand(1..@sides)
+  end
+  erb(:results)
 end
